1626e8c0546f0ba28cfcf0979c6826475049953f
Rynet <109999485+Constantine-Decrypt@users.noreply.github.com>
2023-12-01 15:32:47 -0500

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 	http.HandleFunc("/google-login", handleGoogleLogin)
 	http.HandleFunc("/google-callback", handleGoogleCallback)
 
-	fmt.Println("Server started on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server started on port 8443")
+	log.Fatal(http.ListenAndServe(":8443", nil))
 }
