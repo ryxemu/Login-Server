5ec9ce0f2a8e90a9dd9412f021a0353dbc85d99a
Rynet <109999485+Constantine-Decrypt@users.noreply.github.com>
2023-12-01 14:26:44 -0500

    oath integration test

--- registration.go
+++ registration.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 
-	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 
 	_ "github.com/mattn/go-sqlite3"
 )
-
-type User struct {
-	Username string
-	Password string
-}
 
 var db *sql.DB
 
@@ -29,64 +25,88 @@ func main() {
 
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		username TEXT UNIQUE,
-		password TEXT
+		google_id TEXT,
+		email TEXT,
+		name TEXT
 	)`)
 	if err != nil {
 		log.Fatal("Failed to create table:", err)
 	}
 
-	http.HandleFunc("/register", registerHandler)
-	http.HandleFunc("/registerForm", registerFormHandler)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/google-login", handleGoogleLogin)
+	http.HandleFunc("/google-callback", handleGoogleCallback)
 
 	fmt.Println("Server started on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func registerHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
+	googleOauthConfig := &oauth2.Config{
+		ClientID:     "YOUR_GOOGLE_CLIENT_ID",
+		ClientSecret: "YOUR_GOOGLE_CLIENT_SECRET",
+		RedirectURL:  "http://localhost:8080/google-callback",
+		Scopes:       []string{"profile", "email"},
+		Endpoint:     google.Endpoint,
 	}
 
-	username := r.FormValue("username")
-	password := r.FormValue("password")
+	url := googleOauthConfig.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
 
-	if username == "" || password == "" {
-		http.Error(w, "Username and password are required", http.StatusBadRequest)
-		return
+func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	googleOauthConfig := &oauth2.Config{
+		ClientID:     "YOUR_GOOGLE_CLIENT_ID",
+		ClientSecret: "YOUR_GOOGLE_CLIENT_SECRET",
+		RedirectURL:  "http://localhost:8080/google-callback",
+		Scopes:       []string{"profile", "email"},
+		Endpoint:     google.Endpoint,
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	state := r.FormValue("state")
+	code := r.FormValue("code")
+	token, err := googleOauthConfig.Exchange(r.Context(), code)
 	if err != nil {
-		http.Error(w, "Failed to hash password", http.StatusInternalServerError)
+		http.Error(w, "Failed to exchange token", http.StatusBadRequest)
 		return
 	}
 
-	// Check if username exists
+	client := googleOauthConfig.Client(r.Context(), token)
+	response, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	if err != nil {
+		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
+		return
+	}
+	defer response.Body.Close()
+
+	var userInfo struct {
+		ID    string `json:"id"`
+		Email string `json:"email"`
+		Name  string `json:"name"`
+	}
+	err = json.NewDecoder(response.Body).Decode(&userInfo)
+	if err != nil {
+		http.Error(w, "Failed to decode user info", http.StatusInternalServerError)
+		return
+	}
+
+	// Check if user already exists in the database
 	var count int
-	err = db.QueryRow("SELECT COUNT(id) FROM users WHERE username = ?", username).Scan(&count)
+	err = db.QueryRow("SELECT COUNT(id) FROM users WHERE google_id = ?", userInfo.ID).Scan(&count)
 	if err != nil {
-		http.Error(w, "Failed to check username", http.StatusInternalServerError)
+		http.Error(w, "Failed to check user", http.StatusInternalServerError)
 		return
 	}
 
 	if count > 0 {
-		http.Error(w, "Username already exists", http.StatusBadRequest)
-		return
+		// User exists, log in
+		fmt.Fprintf(w, "User already exists, logged in as %s", userInfo.Name)
+	} else {
+		// Insert new user into the database
+		_, err := db.Exec("INSERT INTO users (google_id, email, name) VALUES (?, ?, ?)", userInfo.ID, userInfo.Email, userInfo.Name)
+		if err != nil {
+			http.Error(w, "Failed to insert user", http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "New user registered: %s", userInfo.Name)
 	}
-
-	_, err = db.Exec("INSERT INTO users (username, password) VALUES (?, ?)", username, hashedPassword)
-	if err != nil {
-		http.Error(w, "Failed to insert user", http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(w, "Registration successful")
-}
-
-func registerFormHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("register.php"))
-	tmpl.Execute(w, nil)
 }
