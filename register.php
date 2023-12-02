ead792026f76e605b823b4e57819923d9ac16b68
Rynet <109999485+Constantine-Decrypt@users.noreply.github.com>
2023-12-01 19:56:58 -0500

    2FA and Database Logic

--- register.php
+++ register.php
@@ -1,79 +1,77 @@
 <?php
 session_start();
 
-if (isset($_SESSION['access_token'])) {
-    header('Location: /dashboard.php');
-    exit();
+require_once 'vendor/autoload.php'; // Include the Google Authenticator library
+
+// Your database connection details
+$servername = 'your_server';
+$username = 'your_username';
+$password = 'your_password';
+$dbname = 'your_database';
+
+// Establish database connection (replace with your database details)
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-if (isset($_GET['code'])) {
-    $code = $_GET['code'];
-    $client_id = 'YOUR_GOOGLE_CLIENT_ID';
-    $client_secret = 'YOUR_GOOGLE_CLIENT_SECRET';
-    $redirect_uri = 'http://yourdomain.com/google-callback.php'; // Replace with your registered callback URI
-
-    $token_url = 'https://oauth2.googleapis.com/token';
-    $params = [
-        'code' => $code,
-        'client_id' => $client_id,
-        'client_secret' => $client_secret,
-        'redirect_uri' => $redirect_uri,
-        'grant_type' => 'authorization_code'
-    ];
-
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $token_url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-    $response = curl_exec($curl);
-    curl_close($curl);
-
-    $data = json_decode($response, true);
-    if (isset($data['access_token'])) {
-        $access_token = $data['access_token'];
-
-        // Get user info from Google
-        $info_url = 'https://www.googleapis.com/oauth2/v2/userinfo?access_token=' . $access_token;
-        $user_info = json_decode(file_get_contents($info_url), true);
-
-        // Connect to SQLite database
-        $db = new SQLite3('test.db');
-        if (!$db) {
-            die("Database connection failed: " . $db->lastErrorMsg());
-        }
-
-        // Check if user already exists in the database
-        $query = "SELECT * FROM users WHERE google_id = '" . $user_info['id'] . "'";
-        $result = $db->query($query);
-        
-        if ($result->fetchArray(SQLITE3_ASSOC)) {
-            $_SESSION['user_id'] = $user_info['id'];
-            header('Location: /dashboard.php');
-            exit();
-        } else {
-            // Insert new user into the database
-            $insertQuery = "INSERT INTO users (google_id, email, name) VALUES ('" . $user_info['id'] . "', '" . $user_info['email'] . "', '" . $user_info['name'] . "')";
-            $result = $db->exec($insertQuery);
-            if (!$result) {
-                echo "Error: " . $db->lastErrorMsg();
-                exit();
-            }
-            $_SESSION['user_id'] = $user_info['id'];
-            header('Location: /dashboard.php');
-            exit();
-        }
-
-        $db->close();
-    } else {
-        echo 'Error while fetching access token.';
-    }
-} else {
-    $client_id = 'YOUR_GOOGLE_CLIENT_ID';
-    $redirect_uri = 'http://yourdomain.com/google-callback.php'; // Replace with your registered callback URI
-
-    $auth_url = 'https://accounts.google.com/o/oauth2/auth?client_id=' . $client_id . '&redirect_uri=' . $redirect_uri . '&response_type=code&scope=email profile';
-    header('Location: ' . $auth_url);
-    exit();
+// Placeholder function for database query to save user information
+function saveUser($conn, $email, $passwordHash)
+{
+    $stmt = $conn->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+    $stmt->bind_param("ss", $email, $passwordHash);
+    $stmt->execute();
+    $stmt->close();
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Handle user registration
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Validate and hash the password
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+    // Save user details to the database
+    saveUser($conn, $email, $passwordHash);
+
+    // Generate and display 2FA setup
+    $google2fa = new \PragmaRX\Google2FA\Google2FA();
+    $secret = $google2fa->generateSecretKey();
+    $_SESSION['user_email'] = $email; // Store the email in the session for later use
+
+    $qrCodeUrl = $google2fa->getQRCodeInline(
+        'YourAppName',
+        $email,
+        $secret
+    );
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register and Setup 2FA</title>
+</head>
+<body>
+    <?php if (isset($qrCodeUrl)): ?>
+        <h1>Register and Enable Two-Factor Authentication (2FA)</h1>
+        <p>Scan the QR code below with Google Authenticator:</p>
+        <img src="<?php echo $qrCodeUrl; ?>" alt="QR Code">
+
+        <form action="verify_2fa.php" method="post">
+            <label for="code">Enter verification code:</label>
+            <input type="text" id="code" name="code" required>
+            <input type="submit" value="Verify">
+        </form>
+    <?php else: ?>
+        <h1>Registration Successful</h1>
+        <p>Your account has been registered. You can now log in.</p>
+    <?php endif; ?>
+</body>
+</html>
